fdf10712616dfb0fc8f7b0b9a6d9d2f22f14873b
Kevin Bajzek <kevin.bajzek@gmail.com>
2020-04-12 20:37:57 -0500

    update

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -21,12 +21,6 @@ class Projects extends Component {
                         githubLink={"https://github.com/kbajzek/forum"}
                         externalLink={"https://forum.kevinbajzek.com"}
                         description={"A web app for social communication and discussion in a community. Create threads, respond to and rate posts, and even send private messages. Uses a custom made markup parser and websockets to see who else is viewing threads that you are."} />
-                    <ProjectCard
-                        image={"./videostreamingservice.png"}
-                        name={"Video Streaming Service"}
-                        githubLink={"https://github.com/kbajzek/video-streaming-service"}
-                        externalLink={"https://video-streaming-service.kevinbajzek.com"}
-                        description={"A web app for streaming video content from filmmakers to users. Uploaded videos are automatically converted to the MPEG-DASH standard. Payment is integrated with Stripe and Material UI themes are used throughout."} />
                 </div>
             </div>
         );
